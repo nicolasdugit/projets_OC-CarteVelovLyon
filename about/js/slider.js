e67e34e44e1f2e2fc4a42712dbfa849f22289eab
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-03-30 11:50:36 +0700

    modif slider html et css creation slider non responsive JS

--- about/js/slider.js
+++ about/js/slider.js
@@ -25,59 +25,25 @@ var diapositives = [
     }
 ]
 
-var instructionElt = document.getElementById("instruction");
-instructionElt.style.overflow = 'hidden';
+var slider =document.getElementById("slider");
+var widthSlider = parseFloat(getComputedStyle(slider).width);
 
-//PARAMETRAGE DE LA DIV SLIDER
-var sliderElt = document.getElementById("slider");
-var nombreDiapo = diapositives.length;
-sliderElt.style.width = nombreDiapo * 100 +"%";
-sliderElt.style.minHeigth = "200px";
-sliderElt.style.display = "flex";
-sliderElt.style.position = "relative";
+var move = document.getElementById("move");
+var widthMove = parseFloat(getComputedStyle(move).width);
 
+var buttonBack = document.getElementById("button-back");
+var buttonForward = document.getElementById("button-forward");
 
-//FONCTION DE CREATION DU TEXTE EXPLICATIF
-function texteExplicatif (titre) {
-	this.titre = titre;
+buttonBack.addEventListener("click", function () {
+	var xMove = parseFloat(getComputedStyle(move).left);
+	if (xMove<0) {
+	move.style.left = xMove + widthSlider + "px";
 }
+})
 
-//INTEGRATION DES DIAPOSITIVES DANS LE SLIDER
-diapositives.forEach( function(diapositive) {
-	var diapoElt = document.createElement("figure");
-	diapoElt.style.backgroundColor = 'grey';
-	diapoElt.style.width =100 / nombreDiapo + '%';
-	diapoElt.style.margin = '0';
-	diapoElt.style.padding = '0';
-	diapoElt.style.display = "flex";
-	diapoElt.style.minHeight = '200px';
-	diapoElt.id = diapositive.id;
-
-
-	var textDiapo = document.createElement("figcaption");
-	textDiapo.textContent= diapositive.titre;
-
-	var imageDiapo = document.createElement("img");
-	imageDiapo.src = diapositive.url;
-	imageDiapo.alt = diapositive.alt;
-	imageDiapo.style.width ='50%';
-	imageDiapo.style.height = "200px";
-	
-	diapoElt.appendChild(imageDiapo);
-	diapoElt.appendChild(textDiapo);
-	sliderElt.appendChild(diapoElt);
-});
-
-
-//CREATION DES BOUTONS D'ACTION
-var divButton = document.createElement("div");
-var buttonBack = document.createElement("button");
-buttonBack.textContent = "Précédent";
-
-var buttonForward = document.createElement("button");
-buttonForward.textContent = "Suivant";
-divButton.appendChild(buttonForward);
-divButton.appendChild(buttonBack);
-
-instructionElt.appendChild(divButton);
-
+buttonForward.addEventListener("click", function () {
+	var xMove = parseFloat(getComputedStyle(move).left);
+	if (Math.abs(parseFloat(getComputedStyle(move).left)) !== (widthMove-widthSlider) ) {
+	move.style.left = xMove - widthSlider + "px";
+}
+})
