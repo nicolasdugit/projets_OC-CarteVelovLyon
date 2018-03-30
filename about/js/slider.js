8d279248b13e2d7105137c024d9205f2d4d7861d
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-03-30 14:11:00 +0700

    slider JS responsive

--- about/js/slider.js
+++ about/js/slider.js
@@ -26,24 +26,36 @@ var diapositives = [
 ]
 
 var slider =document.getElementById("slider");
-var widthSlider = parseFloat(getComputedStyle(slider).width);
+var widthSlider = parseFloat(getComputedStyle(slider).width)
 
 var move = document.getElementById("move");
 var widthMove = parseFloat(getComputedStyle(move).width);
 
+var widthSliderPourcent = (parseFloat(getComputedStyle(slider).width)/widthMove)*100;
+
+
+
 var buttonBack = document.getElementById("button-back");
 var buttonForward = document.getElementById("button-forward");
 
+
+
+	var xMove = 0;
+
 buttonBack.addEventListener("click", function () {
-	var xMove = parseFloat(getComputedStyle(move).left);
 	if (xMove<0) {
-	move.style.left = xMove + widthSlider + "px";
-}
+	move.style.left = xMove + 100 + "%";
+	xMove = xMove + widthSlider/widthMove*diapositives.length*100
+	}
 })
 
+
 buttonForward.addEventListener("click", function () {
-	var xMove = parseFloat(getComputedStyle(move).left);
+	// var xMove = (parseFloat(getComputedStyle(move).left));
+	console.log(xMove);
 	if (Math.abs(parseFloat(getComputedStyle(move).left)) !== (widthMove-widthSlider) ) {
-	move.style.left = xMove - widthSlider + "px";
-}
+	move.style.left = xMove - 100 + "%";
+	xMove = xMove - widthSlider/widthMove*diapositives.length*100;
+console.log(xMove);
+	}
 })
