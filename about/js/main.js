43f08d825306ef1fd8cad5be147d2d4e39a2f301
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-04-03 19:51:29 +0700

    Objet diaporama

--- about/js/main.js
+++ about/js/main.js
@@ -1,26 +1,31 @@
-Diaporama.initDiaporama();
-buttonForward.addEventListener("click", Diaporama.moveForward);
-buttonBack.addEventListener("click", Diaporama.moveBack);
+var diapo1 = Object.create(Diaporama);
+diapo1.initDiaporama(images);
+
+// Diaporama.initDiaporama();
+buttonForward.addEventListener("click", function () {
+	diapo1.moveForward();
+});
+
+buttonBack.addEventListener("click", function () {
+	diapo1.moveBack();
+});
 
 document.onkeydown = function handleKeyDown(e){
 		var key = e.keyCode;
 		var Direction;
 		switch (key){
 			case 37:
-				Direction = "left";
+				diapo1.moveBack()
 				break;
-
 			case 39:
-				Direction = "right";
+				diapo1.moveForward();
 				break;
-				return;
-			default:
 				return;
 		};
-		if (Direction === "left") {
-		Diaporama.moveBack();
-		}
-		if (Direction === "right" ) {
-		Diaporama.moveForward();
-		}
 };
+
+
+
+
+
+
--- about/js/slider.js
+++ about/js/slider.js
@@ -3,26 +3,27 @@ var buttonForward = document.getElementById("button-forward");
 var buttonStop = document.getElementById("button-stop")
 var carrousel = document.getElementById("carrousel");
 
+var imageDiapo = document.getElementById("image-diapo");
+var figcationDiapo = document.getElementById("figcaption-diapo")
+
 // CREATION tableau IMAGE
 var Image = {
 //initialise l'image
-	initImage: function (src, alt, id, figureId, description) {
+	initImage: function (src, alt, description) {
 		this.src = src;
 		this.alt = alt;
-		this.id = id;
-		this.figureId = figureId;
 		this.description = description;
 	}
 };
 
 var image1 = Object.create(Image);
-image1.initImage("images/img1.png","diapo 1", "diapo1", "figure1", "texte explication de la premiere diapo");
+image1.initImage("images/img1.png","diapo 1", "texte explication de la premiere diapo");
 var image2 = Object.create(Image);
-image2.initImage("images/img2.png","diapo 2", "diapo2", "figure2", "texte explication de la deuxieme diapo");
+image2.initImage("images/img2.png","diapo 2", "texte explication de la deuxieme diapo");
 var image3 = Object.create(Image);
-image3.initImage("images/img3.png","diapo 3", "diapo3", "figure3", "texte explication de la troisieme diapo"); 
+image3.initImage("images/img3.png","diapo 3", "texte explication de la troisieme diapo"); 
 var image4 = Object.create(Image);
-image4.initImage("images/img4.png","diapo 4", "diapo4", "figure4", "texte explication de la quatrieme diapo"); 
+image4.initImage("images/img4.png","diapo 4", "texte explication de la quatrieme diapo"); 
 
 // TABLEAU DES IMAGES
 var images = [];
@@ -32,96 +33,50 @@ images.push(image3);
 images.push(image4);
 
 
-//FONCTION CREATION FIGURE
-function creationFigure (image) {
-	var figureElt = document.createElement("figure");
-	figureElt.style.width = "100%";
-	figureElt.style.display = "flex";
-	figureElt.style.justifyContent = 'space-between';
-	figureElt.id = image.id;
-
-	return figureElt;
-}
-
-//FONCTION CREATION IMG
-function creationImage (image) {
-	var imageElt = document.createElement("img");
-	imageElt.style.position = "relative";
-	imageElt.style.width = "50%";
-	imageElt.style.height = "400px";
-	imageElt.src = image.src;
-	imageElt.alt = image.alt;
-	return imageElt;
-}
-
-//FONCTION CREATION FIGCAPTION
-function creationFigcaption (image) {
-	var figcaptionElt = document.createElement("figcaption");
-	figcaptionElt.style.position = "relative";
-	figcaptionElt.textContent = image.description;
-	figcaptionElt.id = image.figureId;
-	return figcaptionElt;
-}
-
-//CREATION DU TABLEAU DES DIAPOS
-var diapos =[];
-
-var i = 0;
-var time = 3000;
-
 var Diaporama = {
 
-	initDiaporama: function() {
+	// INITIALISE LE DIAPORAMA
+	initDiaporama: function(tabImages) {
 
-		images.forEach( function(image) {
-		var figureDiapo = creationFigure(image);
-		var imageDiapo = creationImage (image);
-		var figcaptionDiapo = creationFigcaption (image);
+		this.tabImages = tabImages;
+		this.index = 0;
 
-		figureDiapo.appendChild(imageDiapo);
-		figureDiapo.appendChild(figcaptionDiapo);
-		diapos.push(figureDiapo);
+		tabImages.forEach( function() {
+			imageDiapo.src = tabImages[0].src;
+			imageDiapo.alt = tabImages[0].alt;
+			figcationDiapo.textContent = tabImages[0].description;
 		});
-
-		carrousel.appendChild(diapos[i]);
-		
 	},
 
+	// DIAPORAMA VERS LA DROITE
 	moveForward: function () {
-		clearInterval(Diaporama.moveAuto);
-		if (i< diapos.length -1) {
-		carrousel.innerHTML = "";
-		carrousel.appendChild(diapos[i+1]);
-		i++;
+		if (this.index < this.tabImages.length -1) {
+		this.index++;
+		imageDiapo.src = this.tabImages[this.index].src;
+		imageDiapo.alt = this.tabImages[this.index].alt;
+		figcationDiapo.textContent = this.tabImages[this.index].description;
 		} else {
-			i=0;
-			carrousel.innerHTML = "";
-			carrousel.appendChild(diapos[i]);
+			this.index = 0;
+			imageDiapo.src = this.tabImages[this.index].src;
+			imageDiapo.alt = this.tabImages[this.index].alt;
+			figcationDiapo.textContent = this.tabImages[this.index].description;
 		}
 	},
 
+	// DIAPORAMA VERS LA GAUCHE
 	moveBack: function () {
-		clearInterval(Diaporama.moveAuto);
-		if (i>0){
-		carrousel.innerHTML = "";
-		carrousel.appendChild(diapos[i-1]);
-		i--;
+		if (this.index > 0 ){
+			this.index--;
+			imageDiapo.src = this.tabImages[this.index].src;
+			imageDiapo.alt = this.tabImages[this.index].alt;
+			figcationDiapo.textContent = this.tabImages[this.index].description;
 		} else {
-			i=diapos.length -1;
-			carrousel.innerHTML = "";
-			carrousel.appendChild(diapos[i]);
-		}
-	},
-
-	moveAuto: function (){
-
-		carrousel.innerHTML = "";
-		carrousel.appendChild(diapos[i]);
-		if (i< diapos.length -1) {
-		i++;
-		}  else {
-			i =0;
+			this.index = this.tabImages.length -1;
+			imageDiapo.src = this.tabImages[this.index].src;
+			imageDiapo.alt = this.tabImages[this.index].alt;
+			figcationDiapo.textContent = this.tabImages[this.index].description;
 		}
 	},
 
 };
+
