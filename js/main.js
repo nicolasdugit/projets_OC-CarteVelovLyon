a95804d36a2e4c442f455b5d85ff462f3feaba9b
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-05-05 10:09:28 +0700

    changement image png par jpg

--- js/main.js
+++ js/main.js
@@ -38,13 +38,13 @@ toReservation.addEventListener("click", function () {
 var imagesDiaporama = [];
 // CREATION DES IMAGES A L'AIDE DE L'OBJET IMAGE
 var image1 = Object.create(ImageDiaporama);
-image1.initImage("images/diapo1.png","diapo 1", "Bienvenue sur le site de RésaVélo'V, service de location de velo sur la ville de Lyon. Première visite ? Suivez le guide pas à pas. Utilisez les fleches directionnelles pour actionner le diaporama");
+image1.initImage("images/diapo1.jpg","diapo 1", "Bienvenue sur le site de RésaVélo'V, service de location de velo sur la ville de Lyon. Première visite ? Suivez le guide pas à pas. Utilisez les fleches directionnelles pour actionner le diaporama");
 var image2 = Object.create(ImageDiaporama);
-image2.initImage("images/diapo2.png","diapo 2", "Selectionnez votre station Vélo'V. Les marqueurs verts indiquent une station ouverte avec vélo diponible, les rouges une station ouverte mais sans vélo disponible. Les stations fermées sont repérées par une icone travaux. Une fois la station selectionnée, cliquez sur réserver.");
+image2.initImage("images/diapo2.jpg","diapo 2", "Selectionnez votre station Vélo'V. Les marqueurs verts indiquent une station ouverte avec vélo diponible, les rouges une station ouverte mais sans vélo disponible. Les stations fermées sont repérées par une icone travaux. Une fois la station selectionnée, cliquez sur réserver.");
 var image3 = Object.create(ImageDiaporama);
-image3.initImage("images/diapo3.png","diapo 3", "Vous devez maintenant signer dans la zone de signature pour valider votre réservation."); 
+image3.initImage("images/diapo3.jpg","diapo 3", "Vous devez maintenant signer dans la zone de signature pour valider votre réservation."); 
 var image4 = Object.create(ImageDiaporama);
-image4.initImage("images/diapo4.png","diapo 4", "Votre vélo est maintenant reservé pour une durée de 20 min. Si cette reservation ne vous convient plus, vous pouvez l'annuler. Bonne promenade ! "); 
+image4.initImage("images/diapo4.jpg","diapo 4", "Votre vélo est maintenant reservé pour une durée de 20 min. Si cette reservation ne vous convient plus, vous pouvez l'annuler. Bonne promenade ! "); 
 // MISE EN PLACE DE CHAQUE IMAGE DANS LE TABLEAU IMAGES
 imagesDiaporama.push(image1, image2, image3, image4);
 // CREATION DU SLIDER AVEC L'OBJET DIAPORAMA
